7ee5189b0868db6f9d93b10387b190f20470f6a8
Ernest Busico <Ernest.Busico@academytrainee.com>
2018-07-17 15:37:58 +0100

    Practice

---
+++ Intermediate/src/BlackJClass.java
@@ -0,0 +1,15 @@
+
+public class BlackJClass {
+public int blkjk (int a, int b) {
+	if (a > 21 && 21 <= b) {
+		return b;
+	}
+	else if (a < 21 && 21 >= b){
+		return a;
+	}
+	else {
+		return 0;
+		
+	}
+}
+}
---
+++ Intermediate/src/Blackjack.java
@@ -0,0 +1,13 @@
+
+public class Blackjack {
+
+	public static void main(String[] args) {
+		
+		BlackJClass blkj = new BlackJClass();
+		
+		System.out.println(blkj.blkjk(21, 21));
+
+}
+}
+
+ 
