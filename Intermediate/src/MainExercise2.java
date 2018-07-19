4abc1d4ed92286be228fb0869b59495d09b8961a
Ernest Busico <Ernest.Busico@academytrainee.com>
2018-07-19 17:38:57 +0100

    QAExercises

--- Intermediate/src/MainExercise2.java
+++ Intermediate/src/MainExercise2.java
@@ -1,15 +1,30 @@
 public class MainExercise2 {
 
 	public static void main(String[] args) {
+		//
+		// // BlackJClass blkj = new BlackJClass();
+		// // System.out.println(blkj.blkjk(21, 21));
+		//
+		// UniqueSum uSum = new UniqueSum();
+		// System.out.println(uSum.operateThree(3,3,3));
+		//
+		// // Temperature hot = new Temperature ();
+		// // System.out.println(hot.tooHot(100, true));
+		//
+		//
+		//
+		People[] pArray = new People[5];
+		
+		People p1 = new People("Ernest", 21, "Trainee");
+	
+		
+		
+	
 
-		// BlackJClass blkj = new BlackJClass();
-		// System.out.println(blkj.blkjk(21, 21));
-
-		UniqueSum uSum = new UniqueSum();
-		System.out.println(uSum.operateThree(3,3,3));
-
-		// Temperature hot = new Temperature ();
-		// System.out.println(hot.tooHot(100, true));
+		System.out.println(p1.getName());
+		System.out.println(p1.getAge());
+		System.out.println(p1.getJobTitle());
+		
 
 	}
 }
--- Intermediate/src/People.java
+++ Intermediate/src/People.java
@@ -1,9 +1,26 @@
 
 public class People {
+	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		String name;
+		int age;
+		String jobTitle;
+		
+		
+		public People (String pName, int pAge, String pJobTitle) {
+	
+		this.name = pName;
+		this.age = pAge;
+		this.jobTitle = pJobTitle;
+		}
+		
+	    public String getName() {
+	    return name;
+	    }
+	    public int getAge() {
+	    return age;
+	    }
+	    public String getJobTitle() {
+	    return jobTitle;	
+	    }
 }
